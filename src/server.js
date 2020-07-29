20ff1682a3006e565ac6fdebe730ef8ea89416d7
Michael Hoyer <michaelhoyer@outlook.com>
2020-07-29 11:34:21 +0200

    setup simple server with visjs

---
+++ src/server.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.static('public'));
+app.listen(PORT, () => console.log(`server running on port ${PORT}`));
